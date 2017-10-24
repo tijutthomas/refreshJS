ac1efaebdaa4497d4802051edfa6b3e8b787c939
Tiju T Thomas <tijutthomas@gmail.com>
2017-10-24 17:29:47 -0500

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
+/** Filter */
+
 var arr = [
     {"name":"apple", "count": 2},
     {"name":"orange", "count": 5},
@@ -9,5 +11,20 @@ var newArr = arr.filter(function(item){
     return item.name === "pear";
 });
 
-
 console.log("Filter results:",newArr);
+
+/** forEach */
+
+var arr = [1,2,3,4,5,6,7,8];
+
+// Uses the usual "for" loop to iterate
+for(var i= 0, l = arr.length; i< l; i++){
+	console.log(arr[i]);
+}
+
+console.log("========================");
+
+//Uses forEach to iterate
+arr.forEach(function(item,index){
+	console.log(item);
+});
