34be151841b018c07245fc4cc0b6b29337c59ee6
Tiju T Thomas <tijutthomas@gmail.com>
2017-10-24 17:20:40 -0500

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var arr = [
+    {"name":"apple", "count": 2},
+    {"name":"orange", "count": 5},
+    {"name":"pear", "count": 3},
+    {"name":"orange", "count": 16},
+];
+    
+var newArr = arr.filter(function(item){
+    return item.name === "orange";
+});
+
+
+console.log("Filter results:",newArr);
