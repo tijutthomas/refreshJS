5ba0dc93bfaeb8ed9aa6325c0ba2bde685a8ed1e
Tiju T Thomas <tijutthomas@gmail.com>
2017-10-24 17:57:31 -0500

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -55,3 +55,15 @@ function getNewArr(){
 }
 
 console.log(getNewArr());
+
+/** reduce */
+var arr = ["apple","orange","apple","orange","pear","orange"];
+
+function getWordCnt(){
+    return arr.reduce(function(prev,next){
+        prev[next] = (prev[next] + 1) || 1;
+        return prev;
+    },{});
+}
+
+console.log(getWordCnt());
